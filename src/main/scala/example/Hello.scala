7d7c6db4aaef43e5a8fe0717d71dc8fc73ae72a9
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2022-02-17 11:38:30 +0900

    Add greeting4

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -8,4 +8,5 @@ trait Greeting {
   lazy val greeting: String = "hello" + BuildInfo.toString
   lazy val greeting2: String = "hello2" + BuildInfo.toString
   lazy val greeting3: String = "hello3" + BuildInfo.toString
+  lazy val greeting4: String = "hello4" + BuildInfo.toString
 }
